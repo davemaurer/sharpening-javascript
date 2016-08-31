0d83cdef1647e11246221cdb7a64711f4f6c0cc0
David Maurer <davemaurermail@gmail.com>
2016-08-30 23:19:23 -0600

    Added conditional for a number not divisible by either 3 or 5. Working.

--- fizzbuzz-js/fizzbuzz.js
+++ fizzbuzz-js/fizzbuzz.js
@@ -5,9 +5,12 @@ function fizzBuzz (number) {
     console.log('Fizz...only.');
   } else if(number % 5 === 0) {
     console.log('Buzz...kind of.');
+  } else {
+    console.log('Not even a zzz. Sorry...')
   }
 };
 
 fizzBuzz(15);
 fizzBuzz(9);
 fizzBuzz(10);
+fizzBuzz(2);

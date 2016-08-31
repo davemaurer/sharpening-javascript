430409f56598209acf35d8ab4ea29b452d06c8e6
David Maurer <davemaurermail@gmail.com>
2016-08-30 23:17:28 -0600

    Add conditionals for divisible by 3 and 5. Fizz and Buzz. Working.

--- fizzbuzz-js/fizzbuzz.js
+++ fizzbuzz-js/fizzbuzz.js
@@ -1,7 +1,13 @@
 function fizzBuzz (number) {
   if(number % 3 === 0 && number % 5 === 0) {
     console.log('FizzBuzz Baby!');
+  } else if(number % 3 === 0) {
+    console.log('Fizz...only.');
+  } else if(number % 5 === 0) {
+    console.log('Buzz...kind of.');
   }
 };
 
 fizzBuzz(15);
+fizzBuzz(9);
+fizzBuzz(10);

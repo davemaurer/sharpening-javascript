31590cf35b36d40f2eef86c742e8084e17af273a
David Maurer <davemaurermail@gmail.com>
2016-08-31 22:57:08 -0600

    Passing test for returning Buzz if number divisible by 5.

--- fizzbuzz-js/test/fizzbuzz-test.js
+++ fizzbuzz-js/test/fizzbuzz-test.js
@@ -6,5 +6,10 @@ describe('fizzBuzz', function() {
   it('should return Fizz if the number is divisible by 3', function() {
     assert.equal(fb.fizzBuzzer(3), 'Fizz...only.');
     assert.equal(fb.fizzBuzzer(9), 'Fizz...only.');
-  })
+  });
+
+  it('should return Buzz if the number is divisible by 5', function() {
+    assert.equal(fb.fizzBuzzer(5), 'Buzz...kind of.');
+    assert.equal(fb.fizzBuzzer(25), 'Buzz...kind of.');
+  });
 });

4e819e809c0ed35444f4798d7c77cc84068280b4
David Maurer <davemaurermail@gmail.com>
2016-09-25 21:42:51 -0600

    Reformat recursive fibbo-2 to be more readable.

--- fibonacci-js/fibbo-2.js
+++ fibonacci-js/fibbo-2.js
@@ -3,18 +3,15 @@
 var fibboRecursive = function (n, numbers) {
   var length, lastNumber, secondToLastNumber;
 
-  if (!numbers) {
-    numbers = [0, 1]
-  }
+  if (!numbers) { numbers = [0, 1] }
 
   length = numbers.length;
   lastNumber = numbers[length -1];
   secondToLastNumber= numbers[length -2];
 
-  if (numbers.length >= n) {
-    return lastNumber;
-  }
+  if (numbers.length >= n) { return lastNumber; }
 
   numbers.push(lastNumber + secondToLastNumber);
+
   return fibboRecursive(n, numbers)
 };
